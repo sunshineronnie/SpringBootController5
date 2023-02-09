8e1da3f94b8cc6d3f7f850ded5b7110be1ae9abf
Admin <kqadb@mail.ru>
2023-02-09 16:23:04 +0500

    Initial commit

---
+++ src/main/java/pro/sky/SpringBootController5/SpringBootController5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringBootController5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootController5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootController5Application.class, args);
+	}
+
+}
